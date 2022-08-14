d1bfd39cdaa6fa9597643854385dd6e8a008057b
Angel Duarte <adavid.tinocod@gmail.com>
2022-08-14 13:29:06 -0500

    stack tested c:

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -1,10 +1,29 @@
 package stack;
+
 // Testing file
 public class Main {
-    URL yt = new URL("youtube");
-    URL gh = new URL("github");
-    URL fb = new URL("facebook");
-    URL ig = new URL("instagram");
-    URL gg = new URL("google");
-    StackProcess process = new StackProcess();
+    public static void main(String[] args) {
+        URL yt = new URL("youtube");
+        URL gh = new URL("github");
+        URL fb = new URL("facebook");
+        URL ig = new URL("instagram");
+        URL gg = new URL("google");
+        StackProcess process = new StackProcess();
+
+        process.push(yt);
+        process.push(fb);
+        process.push(ig);
+        process.push(gg);
+        process.push(gh);
+
+        while (!process.isEmpty()) {
+            System.out.println(process.get().getUrl());
+            System.out.println(process.length());
+            process.pop();
+        }
+
+        System.out.println(process.get());
+        System.out.println(process.length());
+        process.pop();
+    }
 }
